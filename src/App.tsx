777f3c0b89acc87dac54b5ebc7df3f605b3e2d7c
Lemmy731 <lemmy.osiki@decagon.dev>
2023-05-24 17:33:09 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Landingpage from './Component/Landingpage';
+
+function App() {
+  return (
+    <>
+    <Landingpage bg={"blue"}/>    
+    <Landingpage bg={"white"}/>  
+    </>
+  );
+}
+
+export default App;
